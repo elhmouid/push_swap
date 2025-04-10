b638faf95363059c9ac856907034ad15fedd6f05
elhmouid <mohamed.elhmouid@edu.uiz.ac.ma>
2025-04-10 19:17:42 +0100

    norm

--- sort_large.c
+++ sort_large.c
@@ -1,19 +1,5 @@
 #include "push_swap.h"
 
-// void set_index(t_list *stack, int index, int min)
-// {
-//     t_list *tmp;
-
-//     tmp = stack;
-//     while (tmp)
-//     {
-//         if (tmp->value == min)
-//             tmp->index = index;
-//         tmp = tmp->next;
-//     }
-// }
-
-
 void index_stack(t_list *stack)
 {
     int s = stack_size(stack);
@@ -23,8 +9,9 @@ void index_stack(t_list *stack)
 
     while (s)
     {
-        int min = INT_MAX;
+        int min;
 
+        min = INT_MAX;
         tmp = stack;
         while (tmp)
         {
@@ -56,27 +43,93 @@ void first_step(t_list **stack_a, t_list **stack_b)
             push(stack_a, stack_b, 'b');
             i++;
         }
-        else if ((*stack_a)->index <= i + 15) {
+        else if ((*stack_a)->index <= i + 15)
+        {
             push(stack_a, stack_b, 'b');
             rotate(stack_b, 'b');
             i++;
         }
-        else {
+        else
             rotate(stack_a, 'a');
-        }
         size--;
     }
-    print_stack(*stack_b);
 }
 
+void	assign_position(t_list *stack_b)
+{
+	int i = 0;
+	t_list *tmp = stack_b;
+	while (tmp)
+	{
+		tmp->position = i;
+		i++;
+		tmp = tmp->next;
+	}
+}
+int	find_position_max(t_list *stack)
+{
+	int		pos;
+	t_list	*current;
 
+	pos = 0;
+	current = stack;
+	while (current)
+	{
+		if (current->value)
+			return (pos);
+		pos++;
+		current = current->next;
+	}
+	return (-1);
+}
+
+void push_back_to_a(t_list **stack_a, t_list **stack_b)
+{
+    int max_index;
+    int max_pos;
+    int b_size;
+    
+    while (*stack_b)
+    {
+        b_size = stack_size(*stack_b);
+        
+        t_list *current = *stack_b;
+        
+        while (current)
+        {
+            if (current->value <= current->next->value)
+			{
+
+			}
+			current = current->next;
+        }
+        // if (max_pos <= b_size / 2)
+        // {
+        //     while (max_pos > 0)
+        //     {
+        //         rotate(stack_b, 'b');
+        //         max_pos--;
+        //     }
+        // }
+        // else
+        // {
+        //     while (max_pos < b_size)
+        //     {
+        //         reverse_rotate(stack_b, 'b');
+        //         max_pos++;
+        //     }
+        // }
+        // push(stack_b, stack_a, 'a');
+		
+    }
+}
 void print_stack(t_list *stack)
 {
     t_list *current = stack;
     
     while (current)
     {
-        printf("Value: %ld, Index: %d\n", current->value, current->index);
+        fprintf(stderr, "Value: %ld, Index: %d\n", current->value, current->index);
         current = current->next;
     }
 }
